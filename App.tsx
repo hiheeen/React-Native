d9383fe3dedbcfe92dddc04529cbe6a7fe377db9
hiheeen <gmldms0338@naver.com>
2024-01-16 14:27:45 +0900

    firebase 첫 설치

--- App.tsx
+++ App.tsx
@@ -6,13 +6,30 @@ import { Provider } from 'react-redux';
 import PostApp from './components/redux-components/PostApp';
 import Counter from './redux-toolkit/Counter';
 import store from './redux/store';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './screens/LoginScreen';
 
 export default function App(): JSX.Element {
+  const Stack = createNativeStackNavigator();
   return (
     <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            options={{ headerShown: false }} // 기본값은 true, 헤더가 표시되고 name이 헤더에 표시됨
+            name="Main"
+            component={MainScreen}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Login"
+            component={LoginScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
       {/* <AuthApp /> */}
       {/* <PostApp /> */}
-      <MainScreen />
     </Provider>
   );
 }
